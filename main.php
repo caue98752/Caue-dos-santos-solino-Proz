8a22882710797133b7f8cccf3d7601f54918d100
caue98752 <caue.solino@gmail.com>
2026-08-05 17:01:24 -0300

    ativiade 1- Operacionais aritimetico

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = (float)readline("Numero 1: ");
+$numero2 = (float)readline("Numero 2: ");
+
+$soma = ($numero1 + $numero2);
+$subtracao = ($numero1 - $numero2);
+
+echo "\n============================\n";
+echo "\n$numero1 + $numero2 = " . $soma . "\n";
+echo "\n$numero1 - $numero2 = " . $subtracao . "\n";
